d2c9b3181e876f9aa7c17784132fc1dc403cc966
minhaj-net <infoglb.net@gmail.com>
2025-12-22 19:41:29 +0600

    fixed a error by route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"], // enable light/dark themes
-  },
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"], // enable light/dark themes
+  },
 };
